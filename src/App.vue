879b57bb823af9ad0b58c9e7ad0bfdc0b9be61ef
CookiesCream1 <luky.pospa04@gmail.com>
2024-02-04 13:45:49 +0100

    amoogus

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Deparements from "./components/Departments.vue";
+import ProductSquare from "./components/ProductSquare.vue";
+</script>
+
+<template>
+  <!--<HelloWorld/>-->
+  <ProductSquare title="air pump" detail-text="pump that pumps some damn air.ahwohaonco ankod naokdn ownknaodnf koanos kdnpo iooijruijfernojsyloiknjvfroij dakn adknaokwnod" :price="5.99" :rating="4.5" image-link="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy3If1UMuNyngrrSSzjPsrdPpRD4Oknl7St1coP86i0J7Q-Npnh9rVeXRB3unQktSJ98U&usqp=CAU"/>
+  <Deparements title="Zamazom Prime" :-deparements="[{name:'penile', checked:true}, {name:'noisy',checked:false}, {name:'will reccumends', checked:true}] "/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Departments.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{
+Deparements: {name:string, checked:boolean}[]
+
+title: string[]
+}>()
+</script>
+
+<template>
+<div class="alignment">
+<h3>{{ title }}</h3>
+<li v-for="department in Deparements">
+    <div style="display:flex">
+        {{ department.name }}
+        <input type="checkbox" :id="department.name" v-model="department.checked">
+        </div>
+</li>
+</div>
+
+
+</template>
+
+<style>
+.alignment{
+    display: flex;
+    flex-direction: column;
+}
+.fix{
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+}
+</style>
